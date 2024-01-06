a787d48f15efcdc49c6903d7d900eaabfa094c33
ArjunB126 <arjunbajare86@gmail.com>
2024-01-06 23:19:30 +0530

    Updates_app.py

--- app.py
+++ app.py
@@ -9,3 +9,20 @@ CORS(app)  # Enable CORS for all routes
 PROJECT_ID = "total-entity-409908"  
 LOCATION = "us-central1"  
 vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+def create_session():
+    chat_model = ChatModel.from_pretrained("chat-bison@001")
+    chat = chat_model.start_chat()
+    return chat
+
+
+def response(chat, message):
+    parameters = {
+         "candidate_count": 1,
+         "max_output_tokens": 1024,
+         "temperature": 0.2,
+         "top_p": 0.8,
+         "top_k": 40
+    }
+    result = chat.send_message(message, **parameters)
+    return result.text
