33f63e8e5031058e4727a7cce4c6f5afe57ccabc
ArjunB126 <arjunbajare86@gmail.com>
2024-01-06 23:20:15 +0530

    Updates_test.py

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ from vertexai.language_models import TextGenerationModel
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "....Add API Key Path ...."
 PROJECT_ID = "total-entity-409908"  
 LOCATION = "us-central1" 
-
 vertexai.init(project=PROJECT_ID, location=LOCATION)
-
 def create_session():
     chat_model = vertexai.language_models.ChatModel.from_pretrained("chat-bison@001")
     chat = chat_model.start_chat()
